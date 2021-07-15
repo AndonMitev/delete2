69752e2ef6709faad43601a687e688421a74c97c
AndonMitev <mitevandon94@gmail.com>
2021-07-15 16:59:01 +0300

    add query

--- my-first-contract/src/contract.rs
+++ my-first-contract/src/contract.rs
@@ -1,3 +1,4 @@
+use crate::msg::{CountResponse, QueryMsg};
 use cosmwasm_std::{
     to_binary, Api, Binary, Env, Extern, HandleResponse, InitResponse, Querier, StdError,
     StdResult, Storage,
@@ -104,4 +105,18 @@ pub fn try_refund<S: Storage, A: Api, Q: Querier>(
     // where this value gonna be locked !?
 
     Ok(HandleResponse::default())
+}
+
+pub fn query<S: Storage, A: Api, Q: Querier>(
+    deps: &Extern<S, A, Q>,
+    msg: QueryMsg,
+) -> StdResult<Binary> {
+    match msg {
+        QueryMsg::GetCount {} => to_binary(&query_count(deps)?),
+    }
+}
+
+fn query_count<S: Storage, A: Api, Q: Querier>(deps: &Extern<S, A, Q>) -> StdResult<CountResponse> {
+    // let state = config_read(&deps.storage).load()?;
+    Ok(CountResponse { count: 1 })
 }
--- my-first-contract/src/msg.rs
+++ my-first-contract/src/msg.rs
@@ -17,3 +17,16 @@ pub enum HandleMsg {
     Claim { secret: String },
     Refund {},
 }
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum QueryMsg {
+    // GetCount returns the current count as a json-encoded number
+    GetCount {},
+}
+
+// We define a custom struct for each query response
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct CountResponse {
+    pub count: i32,
+}
